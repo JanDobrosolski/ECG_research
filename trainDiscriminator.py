9852294639813b1589701eab6a2139aba7d01a6e
JanDobrosolski <jandobrosolski@gmail.com>
2024-05-12 15:46:14 +0200

    implemented discriminator training

--- trainDiscriminator.py
+++ trainDiscriminator.py
@@ -8,6 +8,18 @@ if __name__ == "__main__":
     # Load the dataset
     df = pd.read_csv('dataset.csv')
 
+    impossible_count = len(df[df['rmssdFound']==0])
+    possible_count = len(df[df['rmssdFound']==1])
+
+    if impossible_count < possible_count:
+        sampled_ok_rows = df[df['rmssdFound']==1].sample(impossible_count)
+        df = pd.concat([sampled_ok_rows,df[df['rmssdFound']==0]], ignore_index=True)
+    else:
+        sampled_non_ok_rows = df[df['rmssdFound']==1].sample(possible_count)
+        df = pd.concat([sampled_non_ok_rows,df[df['rmssdFound']==1]], ignore_index=True)
+
+    df = df.sample(frac=1).reset_index(drop=True)
+
     checkpointDir = "tmp/checkpoint"
     modelsDir = "models"
 
@@ -16,14 +28,10 @@ if __name__ == "__main__":
     # Convert 'window' from strings of numbers to numpy arrays
     df['window'] = df['window'].apply(lambda x: np.fromstring(x.replace('[', '').replace(']', ''), sep=','))
 
-    df.drop(df[df['rmssdFound'] == 0].index, inplace=True)
-    # df.drop(df[df['rmssd'] > np.percentile(df['rmssd'], 95)].index, inplace=True)
-    # df.drop(df[df['rmssd'] < np.percentile(df['rmssd'], 5)].index, inplace=True)
-
     X = np.stack(df['window'].values)
     X = np.expand_dims(X, axis=-1)
 
-    X_train, X_temp, y_train, y_temp = train_test_split(X, df['rmssd'].to_numpy(), test_size=0.3, random_state=42)
+    X_train, X_temp, y_train, y_temp = train_test_split(X, df['rmssdFound'], test_size=0.3, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
 
     autoencoder = tf.keras.models.load_model('models/autoencoder.h5')
@@ -33,18 +41,18 @@ if __name__ == "__main__":
     input_shape = X_train.shape[1:]
     input_layer = tf.keras.layers.Input(shape=input_shape)
 
-    # Regression branch
+    # Regression branch converted to classification branch
     x = tf.keras.layers.Flatten()(encoder_output)  # Flatten the output of the encoder
     x = tf.keras.layers.Dense(128, activation='relu')(x)
     x = tf.keras.layers.Dropout(0.5)(x)  # Helps prevent overfitting
-    output_rrsd = tf.keras.layers.Dense(1, activation='linear')(x)  # Linear activation for regression
+    output_rrsd = tf.keras.layers.Dense(1, activation='sigmoid')(x)  # Sigmoid activation for binary classification
 
-    # Full model: input to the autoencoder, output from the regression branch
+    # Full model: input to the autoencoder, output from the classification branch
     model = tf.keras.Model(inputs=autoencoder.input, outputs=output_rrsd)
-    model.compile(optimizer='adam', loss='mse', metrics=['mae']) 
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Setup model checkpoint
-    checkpoint_filepath = os.path.join(checkpointDir, 'rmssd_checkpoint')
+    checkpoint_filepath = os.path.join(checkpointDir, 'discriminator_checkpoint')
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_weights_only=True,
@@ -57,7 +65,7 @@ if __name__ == "__main__":
 
     # Evaluate the model
     test_results = model.evaluate(X_test, y_test)
-    print("Test Results - Loss: ", test_results)
+    print("Test Results - Accuracy: ", test_results[1])
 
     # Optionally, save the model
-    model.save(os.path.join(modelsDir,'rmssd.h5'))
+    model.save(os.path.join(modelsDir,'discriminator.h5'))
